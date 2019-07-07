a485085c436999ac4a79cc7b71a2f381eedb237c
Adam Sarli <adam.sarli@gmail.com>
2019-07-07 11:46:33 -0400

    created login form that redirects to sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < ApplicationController
   def new
     @teacher = Teacher.new
   end
+
+  def create
+    binding.pry 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :klasses
   resources :teachers
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
