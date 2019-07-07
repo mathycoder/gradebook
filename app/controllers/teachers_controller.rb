ce295f4d9797a17ff7555b5969873a823eafdb61
Adam Sarli <adam.sarli@gmail.com>
2019-07-07 11:16:09 -0400

    controller action teacher/create and teacher/show

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,22 @@ class TeachersController < ApplicationController
   def new
     @teacher = Teacher.new
   end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      teacher_path(@teacher)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end 
+
+  private
+
+    def teacher_params
+      params.require(:teacher).permit(:name, :grade, :class)
+    end
 end
