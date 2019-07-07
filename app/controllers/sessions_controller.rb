74357b28a2592abc8e5003e3ab27e591eb51756d
Adam Sarli <adam.sarli@gmail.com>
2019-07-07 11:51:35 -0400

    sessions#create logs a user in with an existing email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry 
+    @teacher = Teacher.find_by(email: params[:teacher][:email])
+    if @teacher
+      session[:user_id] = @teacher.id
+      redirect_to(teacher_path(@teacher))
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    # def teacher_params
+    #   params.require(:teacher).permit(:email)
+    # end
 end
