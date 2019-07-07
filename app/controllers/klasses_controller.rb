733610dbc4223265ddf018d5870b05b71b888c06
Adam Sarli <adam.sarli@gmail.com>
2019-07-07 13:35:25 -0400

    can now add a class; redirects to class show page

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -3,5 +3,24 @@ class KlassesController < ApplicationController
   def new
     @klass = Klass.new
   end
-  
+
+  def create
+    @klass = Klass.new(klass_params)
+    if @klass.save
+      redirect_to(klass_path(@klass))
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @klass = Klass.find(params[:id])
+  end
+
+  private
+
+    def klass_params
+      params.require(:klass).permit(:name, :subject, :grade, :period)
+    end
+
 end
