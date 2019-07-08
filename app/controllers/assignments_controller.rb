8dae6f7682063fcd83564f73a27f49b3ec4a1097
Adam Sarli <adam.sarli@gmail.com>
2019-07-08 16:07:56 -0400

    assignments/create creates assignment with an assigned learning target

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,19 +7,18 @@ class AssignmentsController < ApplicationController
 
   def create
     @klass = Klass.find(params[:klass_id])
-    binding.pry
-    # @assignment = @klass.assignments.build(assignment_params)
-    # if @assignment.save
-    #   redirect_to(klass_path(@klass))
-    # else
-    #   render 'new'
-    # end
+    @assignment = Assignment.new(assignment_params)
+    if @assignment.save
+      redirect_to(klass_path(@klass))
+    else
+      render 'new'
+    end
   end
 
   private
 
     def assignment_params
-      params.require(:assignment).permit(:name, :learning_target)
+      params.require(:assignment).permit(:name, :learning_target_id)
     end
 
 end
