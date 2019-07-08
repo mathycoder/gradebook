20e6a5519abe303b3d0d864467ef91e86e3f9add
Adam Sarli <adam.sarli@gmail.com>
2019-07-07 21:34:35 -0400

    Created Student#full_name method and applied it the the Klass Show page

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,9 @@ class Student < ApplicationRecord
   has_many :klass_students
   has_many :klasses, through: :klass_students
   has_many :teacher_klasses, through: :klasses
-  has_many :teachers, through: :teacher_klasses 
+  has_many :teachers, through: :teacher_klasses
+
+  def full_name
+    "#{self.last_name}, #{self.first_name}"
+  end
 end
