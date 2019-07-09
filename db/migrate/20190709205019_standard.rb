7c5645c037a04d88622bce64c46ab8669ef89948
Adam Sarli <adam.sarli@gmail.com>
2019-07-09 16:52:30 -0400

    created Standard table, migration, and model

---
+++ db/migrate/20190709205019_standard.rb
@@ -0,0 +1,9 @@
+class Standard < ActiveRecord::Migration[5.2]
+  def change
+    create_table :standards do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_172308) do
+ActiveRecord::Schema.define(version: 2019_07_09_205019) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 2019_07_08_172308) do
 
   create_table "learning_targets", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "standards", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
