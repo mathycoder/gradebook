4fcc1eeeb4996a560d454d274176934dfb028104
Adam Sarli <adam.sarli@gmail.com>
2019-07-09 13:45:57 -0400

    learning_targets#chronological_assignments sorts assignments by date within an LT

--- app/models/learning_target.rb
+++ app/models/learning_target.rb
@@ -3,5 +3,10 @@ class LearningTarget < ApplicationRecord
   has_many :klasses, through: :klass_learning_targets
 
   has_many :assignments
-  has_many :grades, through: :assignments 
+  has_many :grades, through: :assignments
+
+  def chronological_assignments
+    self.assignments.sort_by{|assignment| assignment.date}
+  end
+
 end
