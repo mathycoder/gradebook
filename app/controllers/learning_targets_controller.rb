cdd7ec81b9a56f18320d73a4ce76fdd0076efd7c
Adam Sarli <adam.sarli@gmail.com>
2019-07-09 11:36:19 -0400

    learning_targets#create adds learning target to a klass

--- app/controllers/learning_targets_controller.rb
+++ app/controllers/learning_targets_controller.rb
@@ -1,6 +1,25 @@
 class LearningTargetsController < ApplicationController
 
   def new
+    @klass = Klass.find(params[:klass_id])
+    @lt = LearningTarget.new
   end
-  
+
+  def create
+    @klass = Klass.find(params[:klass_id])
+    @lt = LearningTarget.new(lt_params)
+    if @lt.save
+      @klass.learning_targets << @lt
+      redirect_to(klass_path(@klass))
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def lt_params
+      params.require(:learning_target).permit(:name)
+    end
+
 end
