5099d590d5663dbc43bf330473d90ed1e196c145
Adam Sarli <adam.sarli@gmail.com>
2019-07-15 13:44:59 -0400

    Added application helper #error_display that grabs #errors.full_messages for resources passed in as an argument

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def error_display(resource)
+    content_tag(:ul) do
+      error_display_lis(resource)
+    end
+  end
+
+  def error_display_lis(resource)
+    if resource.errors.any?
+      html_string = ""
+      resource.errors.full_messages.each do |message|
+        html_string << content_tag(:li, message)
+      end
+    html_string.html_safe
+    end
+  end
 end
