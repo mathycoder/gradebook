04c833e9c9d95668180ffa474fadaf3999b3aef9
Adam Sarli <adam.sarli@gmail.com>
2019-07-18 18:24:36 -0400

    wrapped divs into links on the gradebook page instead of the text

--- app/helpers/klasses_helper.rb
+++ app/helpers/klasses_helper.rb
@@ -1,3 +1,13 @@
 module KlassesHelper
 
+  def student_link_td(student)
+    html_string = content_tag(:td, class: "student-name") do
+      content_tag(:div, class: "student-column") do
+        student.full_name
+      end
+    end
+  end
+
+
+
 end
