672ae7407fe2f02e6d2279d5356e7163d604d752
Adam Sarli <adam.sarli@gmail.com>
2019-07-19 19:04:07 -0400

    Put header_when_logged_in and header_when_logged_out into partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     Teacher.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
   end
 
 end
