1f16d506ffb595de111a8e728bbb967aa9bae45b
Adam Sarli <adam.sarli@gmail.com>
2019-07-19 19:33:49 -0400

    Refactoring header partial; created helpers for LT and class dropdowns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,25 @@ module ApplicationHelper
     end
   end
 
+  def current_klass(klass)
+    klass ? (klass) : (current_user.klasses.build)
+  end
+
+  def klass_dropdown(klass)
+    form_for([:klass, current_klass(klass)], url: klass_redirect_path(), method: :get) do |f|
+      f.collection_select(:id, current_user.klasses_in_alphabetical_order, :id, :klass_name, {include_blank: "Classes"}, {class: "select-blend", onchange: "this.form.submit();"})
+    end
+  end
+
+  def current_lt(klass, lt)
+    lt ? (current_lt = lt) : (current_lt = klass.learning_targets.build)
+  end
+
+  def lt_dropdown(klass, lt)
+    form_for([:learning_target, current_lt(klass, lt)], url: lt_redirect_path(klass), method: :get) do |f|
+      f.collection_select(:name, klass.learning_targets, :name, :name, {include_blank: "All Learning Targets", prompt: "Learning Targets"}, {class: "select-blend", onchange: "this.form.submit();"})
+    end
+  end
+
 
 end
