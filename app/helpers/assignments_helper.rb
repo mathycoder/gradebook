ba005b2b18fe1cc4615132ace4bec43cb4a13466
Adam Sarli <adam.sarli@gmail.com>
2019-07-23 15:11:17 -0400

    refactored assignments/_form with helpers

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -14,4 +14,24 @@ module AssignmentsHelper
   end
 
 
+  def display_field_for_grades(f, student, assignment)
+    content_tag(:tr) do
+      f.fields_for(:grades, student.current_grade(assignment), include_id: false) do |br|
+        student_name_and_score_field(student, br)
+      end
+    end
+  end
+
+
+  def student_name_and_score_field(student, br)
+     content_tag(:div) do
+       html_string = ""
+       html_string << content_tag(:td, br.label(:score, student.full_name))
+       html_string << content_tag(:td, br.label(:score, br.text_field(:score, {class: "text-field-grade", maxlength: "3"})))
+       html_string << br.hidden_field(:student_id, :value => student.id)
+       html_string.html_safe
+     end
+  end
+
+
 end
