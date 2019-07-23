f05876e1e3870fe2ff363749cbe8477b2fa423c3
Adam Sarli <adam.sarli@gmail.com>
2019-07-23 19:34:13 -0400

    Sessions login works when you select the teacher button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,22 @@ class SessionsController < ApplicationController
   before_action :already_logged_in, only: [:new]
 
   def new
-    @teacher = Teacher.new
+    @session = Session.new
   end
 
   def create
-    binding.pry
-    @teacher = Teacher.find_by(email: params[:teacher][:email])
-    if @teacher.nil?
-      redirect_to(login_path, alert: "No account matches that email") if !@teacher
-    elsif @teacher.authenticate(params[:teacher][:password])
-      session[:user_id] = @teacher.id
-      redirect_to(klasses_path)
+    if params[:session][:type] == "teacher"
+      @teacher = Teacher.find_by(email: params[:session][:email])
+      if @teacher.nil?
+        redirect_to(login_path, alert: "No account matches that email") if !@teacher
+      elsif @teacher.authenticate(params[:session][:password])
+        session[:user_id] = @teacher.id
+        redirect_to(klasses_path)
+      else
+        redirect_to(login_path, alert: "Incorrect password")
+      end
     else
-      redirect_to(login_path, alert: "Incorrect password")
+      redirect_to(login_path, alert: "You can only login as a Teacher currently")
     end
   end
 
