8fa2395f01b0e813eaf67b4036d568f6048e649d
Adam Sarli <adam.sarli@gmail.com>
2019-07-23 19:08:22 -0400

    Added admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_182459) do
+ActiveRecord::Schema.define(version: 2019_07_23_230707) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "assignments", force: :cascade do |t|
     t.string "name"
