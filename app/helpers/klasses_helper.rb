5270171cb3e9904da08e86fed3b897370c3134fe
Adam Sarli <adam.sarli@gmail.com>
2019-07-23 12:53:59 -0400

    refactored _table_header_row into helpers

--- app/helpers/klasses_helper.rb
+++ app/helpers/klasses_helper.rb
@@ -8,6 +8,12 @@ module KlassesHelper
     end
   end
 
+  def learning_target_link(klass, target)
+    link_to(klass_learning_target_path(klass, target)) do
+      learning_target_text(target)
+    end
+  end
+
   def learning_target_text(target)
     content_tag(:div, class: "lt-target-label") do
       block = ""
@@ -15,6 +21,16 @@ module KlassesHelper
       block << tag(:br)
       block << target.name
       block.html_safe
+    end
+  end
+
+  def assignment_label
+    content_tag(:div, "Assignments", class: "assignment-label")
+  end
+
+  def klass_title(klass)
+    content_tag(:th, rowspan: "2") do
+      render partial: "klasses/klass_title", locals: {klass: klass}
     end
   end
 
