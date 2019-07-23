94eaf8fbf1b1ad1cd5708c07e99f6765028ee017
Adam Sarli <adam.sarli@gmail.com>
2019-07-23 07:19:27 -0400

    Refactored teacher controller with private methods

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,7 @@
 class TeachersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+  before_action :find_teacher, only: [:show, :update, :destroy]
+  before_action :get_uploaded_io, only: [:create, :update]
 
   def new
     @teacher = Teacher.new
@@ -8,12 +10,8 @@ class TeachersController < ApplicationController
   def create
     @teacher = Teacher.new(teacher_params)
     if @teacher.save
-      uploaded_io = params[:teacher][:picture_url]
-      File.open(Rails.root.join('app', 'assets', 'images', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-        file.write(uploaded_io.read)
-      end
-      @teacher.picture_url = "uploads/#{uploaded_io.original_filename}"
-      @teacher.save
+      upload_pic_to_server()
+      update_teacher_pic()
       session[:user_id] = @teacher.id
       redirect_to(klasses_path(), alert: "New account created")
     else
@@ -22,23 +20,16 @@ class TeachersController < ApplicationController
   end
 
   def show
-    @teacher = Teacher.find(params[:id])
   end
 
   def update
-    @teacher = Teacher.find(params[:id])
-    uploaded_io = params[:teacher][:picture_url]
-    File.delete(Rails.root.join('app', 'assets', 'images', @teacher.picture_url))
-    File.open(Rails.root.join('app', 'assets', 'images', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-    @teacher.picture_url = "uploads/#{uploaded_io.original_filename}"
-    @teacher.save
+    delete_pic_from_server()
+    upload_pic_to_server()
+    update_teacher_pic()
     redirect_to(teacher_path(@teacher), alert: "Profile pic updated")
   end
 
   def destroy
-    @teacher = Teacher.find(params[:id])
     @teacher.destroy
     session.clear
     redirect_to(login_path)
@@ -46,7 +37,29 @@ class TeachersController < ApplicationController
 
   private
 
+    def find_teacher
+      @teacher = Teacher.find(params[:id])
+    end
+
+    def get_uploaded_io
+      @uploaded_io = params[:teacher][:picture_url]
+    end
+
     def teacher_params
       params.require(:teacher).permit(:name, :email, :password, :password_confirmation, :picture_url)
+    end
+
+    def delete_pic_from_server()
+      File.delete(Rails.root.join('app', 'assets', 'images', @teacher.picture_url))
+    end
+
+    def upload_pic_to_server()
+      File.open(Rails.root.join('app', 'assets', 'images', 'uploads', @uploaded_io.original_filename), 'wb') do |file|
+        file.write(@uploaded_io.read)
+      end
+    end
+
+    def update_teacher_pic()
+      @teacher.update(picture_url: "uploads/#{@uploaded_io.original_filename}")
     end
 end
