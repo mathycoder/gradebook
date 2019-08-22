9fd3493d771f9669b4cd0b7fb97963f7af1e345f
Adam Sarli <adam.sarli@gmail.com>
2019-08-22 16:34:16 -0400

    JS testing() conditionally sets background color for assignment averages

--- app/assets/javascripts/formatting.js
+++ app/assets/javascripts/formatting.js
@@ -1,6 +1,15 @@
 function testing() {
-  const allTds = document.querySelectorAll('td')
+  const allTds = document.querySelectorAll('.assignment-averages')
   for (let i=0; i<=allTds.length; i++) {
-    allTds[i].style.color = "blue"
+    if (allTds[i] !== undefined) {
+      const num = allTds[i].querySelector('strong').innerHTML
+      if (parseFloat(num) >= 3.0) {
+        allTds[i].style.backgroundColor = "green"
+      } else if (parseFloat(num) >= 2.0) {
+        allTds[i].style.backgroundColor = "yellow"
+      } else {
+        allTds[i].style.backgroundColor = "red"
+      }
+    }
   }
 }
