6eaec609297941eea5285675bf9ac574cbe1d8b8
Adam Sarli <adam.sarli@gmail.com>
2019-08-23 11:09:03 -0400

    adjusted colors of conditional formatting

--- app/assets/javascripts/formatting.js
+++ app/assets/javascripts/formatting.js
@@ -2,20 +2,23 @@
 //load a new page.  Otherwise, testing() only invokes during the initial page load.
 
 $(document).on('turbolinks:load', function() {
-  testing()
+  conditionalFormatting()
 })
 
-function testing() {
-  const allTds = document.querySelectorAll('.assignment-averages')
+function conditionalFormatting() {
+  const allTds = $('.assignment-averages')
   for (let i=0; i<=allTds.length; i++) {
     if (allTds[i] !== undefined) {
       const num = allTds[i].querySelector('strong').innerHTML
       if (parseFloat(num) >= 3.0) {
-        allTds[i].style.backgroundColor = "rgb(171, 220, 165, 0.5)"
+        allTds[i].style.backgroundColor = "rgb(88, 169, 106, 0.6)"
+        allTds[i].style.color = "black"
       } else if (parseFloat(num) >= 2.0) {
-        allTds[i].style.backgroundColor = "rgb(220, 219, 165, 0.5)"
+        allTds[i].style.backgroundColor = "rgb(212, 208, 96, 0.6)"
+        allTds[i].style.color = "black"
       } else {
-        allTds[i].style.backgroundColor = "rgb(220, 183, 165, 0.5)"
+        allTds[i].style.backgroundColor = "rgb(212, 106, 96, 0.6)"
+        allTds[i].style.color = "black"
       }
     }
   }
