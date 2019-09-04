7a423b2c4dc1ec9e48c4e3fb469a1a5398a90982
Adam Sarli <adam.sarli@gmail.com>
2019-09-03 20:41:49 -0400

    Klass Serializer grabs all relevant info on students, LTs, teachers, assignments, and grades

--- app/serializers/klass_serializer.rb
+++ app/serializers/klass_serializer.rb
@@ -1,4 +1,8 @@
 class KlassSerializer < ActiveModel::Serializer
   attributes :id, :name, :subject, :grade, :period
-  
+  has_many :students
+  has_many :learning_targets
+  has_many :teachers
+  has_many :assignments
+  has_many :grades, through: :assignments
 end
