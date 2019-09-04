550e688f7ed71bcdeed9949db2aaeda2a885e776
Adam Sarli <adam.sarli@gmail.com>
2019-09-04 11:11:23 -0400

    created JSON model object with JS Class LearningTarget

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,9 +5,25 @@ $(document).on('turbolinks:load', function() {
   }
 })
 
+const learningTargets = []
+let learningTargetId = 0
+
+class LearningTarget {
+  constructor(attributes){
+    this.id = learningTargetId++
+    this.name = attributes.name
+    this.standard_id = attributes.standard_id
+    this.created_at = attributes.created_at
+  }
+}
+
 function getData() {
   const klassId = window.location.href.split("/")[4]
   $.get('/classes/' + klassId + '.json', function(json){
     console.log(json)
+    for (i = 0; i<json.learning_targets.length; i++){
+      const lt = new LearningTarget(json.learning_targets[i])
+      learningTargets.push(lt)
+    }
   })
 }
